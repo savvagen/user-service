298f9a8c01b68c4f9f9f2aec16e18dd1456c5d55
savva <genchevskiy.gepur@gmail.com>
2017-10-26 12:19:42 +0300

    .

--- src/main/java/com/example/Controller/SignInController.java
+++ src/main/java/com/example/Controller/SignInController.java
@@ -2,6 +2,7 @@ package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,8 @@ public class SignInController {
     public String openLoginPage(){
         return "login";
     }
+
+
 
 
 }
